acd3c632af59a012b8e486f4300dbe6c4b5b9ff3
Clarissa Forbes <53020872+caforbes@users.noreply.github.com>
2024-09-12 13:08:11 -0700

    update API docs

--- api/main.py
+++ api/main.py
@@ -1,5 +1,5 @@
-from typing import Optional
-from fastapi import FastAPI, HTTPException
+from typing import Annotated
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 import datetime as dt
 
@@ -23,33 +23,40 @@ app = FastAPI()
 configure(app, backend.SETTINGS)
 
 
+# Parameters
+
+user_expiry_type = Query(description="a timezone-aware ISO time string")
+
+
 # Routes
 
 
-@app.get("/")
+@app.get("/", summary="Health check")
 def read_root():
-    """Basic healthcheck."""
+    """Check if the server is running."""
     return "API server is running!"
 
 
-@app.get("/today")
-def read_today(expire: Optional[dt.time] = None) -> Daylist:
+@app.get("/today", summary="Read today's todo items")
+def read_today(expire: Annotated[dt.time | None, user_expiry_type] = None) -> Daylist:
     """Read the current list of things to do today.
 
-    Contains a list of pending tasks and done tasks. This list expires within a 24-hour
-    window of creation.
+    Contains a list of pending tasks and done tasks. This list expires within 24 hours.
+    By default, expires at midnight UTC, or you can provide a custom expiration time
+    that will be used if a new list needs to be created today.
     """
     if expire and not expire.tzinfo:
         raise HTTPException(status_code=400, detail="Timezone not provided.")
     return backend.temp_get_or_make_todaylist(expire)
 
 
-@app.get("/agenda")
-def read_agenda(expire: Optional[dt.time] = None) -> Agenda:
+@app.get("/agenda", summary="Read today's agenda")
+def read_agenda(expire: Annotated[dt.time | None, user_expiry_type] = None) -> Agenda:
     """Read a timeline of what to do next.
 
-    Contains a timeline and indicates the overall finish time. Includes warnings for if
-    the timeline exceeds the daily list expiry time.
+    Contains a timeline and indicates the overall finish time. Includes indications if
+    the timeline exceeds the expiry time of today's list. You can provide a custom
+    expiration time that will be used if a new list needs to be created today.
     """
     if expire and not expire.tzinfo:
         raise HTTPException(status_code=400, detail="Timezone not provided.")
