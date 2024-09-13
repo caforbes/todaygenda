3e73f02bbd7eff7ebc256f6c6e575c43902f34f4
Clarissa Forbes <53020872+caforbes@users.noreply.github.com>
2024-09-13 12:05:08 -0700

    type hints for db methods

--- db/connect.py
+++ db/connect.py
@@ -6,7 +6,11 @@ _this_dir = os.path.dirname(os.path.abspath(__file__))
 _query_module_path = os.path.join(_this_dir, "queries")
 
 
-def query_connect(url: str) -> pugsql.compiler.Module:
+class DBQueriesWrapper(pugsql.compiler.Module):
+    pass
+
+
+def query_connect(url: str) -> DBQueriesWrapper:
     queries = pugsql.module(_query_module_path)
     queries.connect(url)
     return queries
